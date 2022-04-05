00fa33065add90948a87a8d9733e81c00a50e58b
eva.wang <eva.wang81@gmail.com>
2022-04-05 09:38:59 +0800

    added shuffledDeck

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
-const generateRandomNum = (max) => Math.floor(Math.random() * max + 1);
+/*
+=====================================
+Shuffled Deck
+=====================================
+*/
+const generateRandomNum = (max) => Math.floor(Math.random() * max);
 
 const shuffleCards = (cards) => {
-  let randomNum = generateRandomNum();
-  for (let i = 0; i < cards.length; i += 1) {
-    let currentCard = cards[i];
+  for (let currentIndex = 0; currentIndex < cards.length; currentIndex += 1) {
+    let randomNum = generateRandomNum(cards.length);
     let randomCard = cards[randomNum];
-    cards[i] = randomCard;
-    cards[randomCard] = currentCard;
+    let currentCard = cards[currentIndex];
+    cards[currentIndex] = randomCard;
+    cards[randomNum] = currentCard;
   }
   return cards;
 };
 
-const makeDeck = () => {};
+const makeDeck = () => {
+  let deck = [];
+  const suits = ["clubs", "diamonds", "hearts", "spades"];
+  const suitsSymbol = ["♣︎", "♦︎", "♥︎", "♣︎"];
+  for (let suitIdx = 0; suitIdx < suits.length; suitIdx += 1) {
+    const currentSuit = suits[suitIdx];
+    const currentSuitSymbol = suitsSymbol[suitIdx];
+    for (let rankCounter = 1; rankCounter <= 13; rankCounter += 1) {
+      let cardName = `${rankCounter}`;
+      if (cardName === "1") cardName = "ace";
+      if (cardName === "11") cardName = "jack";
+      if (cardName === "12") cardName = "queen";
+      if (cardName === "13") cardName = "king";
+
+      const card = {
+        name: cardName,
+        suit: currentSuit,
+        suitsSymbol: currentSuitSymbol,
+        rank: rankCounter,
+      };
+      deck.push(card);
+    }
+  }
+  // console.log(deck);
+  return deck;
+};
+
+const shuffledDeck = shuffleCards(makeDeck());
+
+/*
+=====================================
+
+=====================================
+*/
