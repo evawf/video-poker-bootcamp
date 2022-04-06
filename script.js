ea45d9728afb47da80158672c86486320ccb5bec
eva.wang <eva.wang81@gmail.com>
2022-04-06 17:36:04 +0800

    added global variables

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 /*
 =====================================
+Global variables
+=====================================
+*/
+const scoreBoardDiv = document.createElement("div");
+scoreBoardDiv.className = "scoreBoardDiv";
+scoreBoardDiv.innerText = "Score Board";
+
+const handDiv = document.createElement("div");
+handDiv.className = "handDiv";
+handDiv.innerText = "Hand";
+
+const pointDiv = document.createElement("div");
+pointDiv.className = "pointDiv";
+pointDiv.innerText = "Coints";
+
+const betBtnDiv = document.createElement("div");
+betBtnDiv.className = "betBtnDiv";
+const btn1 = document.createElement("button");
+btn1.className = "betBtn";
+const btn2 = document.createElement("button");
+btn2.className = "betBtn";
+const btn3 = document.createElement("button");
+btn3.className = "betBtn";
+const btn4 = document.createElement("button");
+btn4.className = "betBtn";
+const btn5 = document.createElement("button");
+btn5.className = "betBtn";
+btn1.innerText = "10";
+btn2.innerText = "20";
+btn3.innerText = "30";
+btn4.innerText = "40";
+btn5.innerText = "50";
+
+betBtnDiv.append(btn1, btn2, btn3, btn4, btn5);
+
+const dealBtn = document.createElement("div");
+dealBtn.className = "dealBtn";
+dealBtn.id = "dealBtn";
+dealBtn.innerText = "Deal";
+
+const mainContainer = document.querySelector(".container");
+mainContainer.innerText = "Hello world!";
+document.body.append(scoreBoardDiv, handDiv, betBtnDiv, dealBtn);
+
+/*
+=====================================
 Shuffled Deck
 =====================================
 */
@@ -43,10 +89,25 @@ const makeDeck = () => {
   return deck;
 };
 
-const shuffledDeck = shuffleCards(makeDeck());
+const newDeck = shuffleCards(makeDeck());
 
 /*
 =====================================
-
+Game logic
 =====================================
 */
+const playerHand = [];
+let coins = 100;
+
+for (let i = 0; i < 5; i += 1) {
+  playerHand.push(newDeck.pop());
+}
+
+const calcHandScore = (playerHand) => {
+  console.log(playerHand);
+  let score = 0;
+
+  return score;
+};
+
+// calcHandScore(playerHand);
